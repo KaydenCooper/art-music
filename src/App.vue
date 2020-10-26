6f7a30714a316e2cabbd6934e4609c093f1185b7
Kayden Cooper <kayden_cooper@yahoo.com>
2020-10-26 16:18:55 -0600

    initial project img added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app row container-fluid px-0">
+    <div class="col-12 px-0 d-flex flex-row overflow">
+      <img id="scroll" src="./assets/picture.jpg" alt="" />
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: "app",
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+img {
+  height: 45rem;
+  animation-direction: normal;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+::-webkit-scrollbar {
+  display: none;
+}
+.overflow {
+  overflow: auto;
+  flex-wrap: nowrap;
+}
+body {
+  margin: 0;
 }
 </style>
