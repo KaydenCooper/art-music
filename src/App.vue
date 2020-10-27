b1708dbd3a7f3fe787c2918ce513cfd6bb0b62eb
Kayden Cooper <kayden_cooper@yahoo.com>
2020-10-27 00:08:26 -0600

    sound and image playing together in sync

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app row container-fluid px-0">
-    <div class="col-12 px-0 d-flex flex-row overflow">
-      <img id="scroll" src="./assets/picture.jpg" alt="" />
+    <div id="scroll" class="col-12 px-0 d-flex flex-row overflow">
+      <div class="col-12 text-center p-2">
+        <button class="btn btn-dark rounded px-4" v-on:click="play">
+          Play
+        </button>
+        <audio class="fixed-bottom" ref="audio" @click="animationStart">
+          <source src="./assets/sound.mp3" type="audio/mp3" />
+        </audio>
+      </div>
     </div>
   </div>
 </template>
@@ -9,13 +16,41 @@
 <script>
 export default {
   name: "app",
+  methods: {
+    play() {
+      let audio = this.$refs.audio;
+      if (audio.paused) {
+        audio.play();
+        document.getElementById("scroll").style.animationPlayState = "running";
+      } else {
+        audio.pause();
+        document.getElementById("scroll").style.animationPlayState = "paused";
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-img {
-  height: 45rem;
-  animation-direction: normal;
+@keyframes animatedBackground {
+  from {
+    background-position: 0px 0;
+  }
+  to {
+    background-position: -1550px 0;
+  }
+}
+#scroll {
+  height: 100vh;
+  width: 100%;
+  background-image: url("./assets/picture.jpg");
+  background-size: cover;
+  animation: animatedBackground 212s linear infinite;
+  animation-play-state: paused;
+}
+audio {
+  height: 20px;
+  width: inherit;
 }
 ::-webkit-scrollbar {
   display: none;
